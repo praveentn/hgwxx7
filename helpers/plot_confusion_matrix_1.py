232fe9fd4b3a87e3bb446ee75a303aad3cfd9fa0
Praveen Narayan <tnpraveen87@gmail.com>
2018-08-29 13:50:11 +0530

    Create plot_confusion_matrix_1.py

---
+++ helpers/plot_confusion_matrix_1.py
@@ -0,0 +1,53 @@
+# plot confusion matrix
+
+from sklearn.metrics import confusion_matrix
+import itertools
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Oranges):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    Source: http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.figure(figsize = (10, 10))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title, size = 24)
+    plt.colorbar(aspect=4)
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45, size = 14)
+    plt.yticks(tick_marks, classes, size = 14)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    
+    # Labeling the plot
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt), fontsize = 20,
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+        
+    plt.grid(None)
+    plt.tight_layout()
+    plt.ylabel('True label', size = 18)
+    plt.xlabel('Predicted label', size = 18)
+
+
+cm = confusion_matrix(y_true, y_pred)
+
+plot_confusion_matrix(cm, classes = ['class 1', 'class 2', 'class 3', 'class 4'],
+                      title = 'Confusion Matrix')
+
+# normalized
+plot_confusion_matrix(cm, classes = ['class 1', 'class 2', 'class 3', 'class 4'],
+                      title = 'Confusion Matrix', normalize=True)
