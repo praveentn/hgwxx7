0cad93cac8d8ca9ccf36295c4cb7d3393f8c450f
Praveen Narayan <tnpraveen87@gmail.com>
2018-08-29 16:05:48 +0530

    Update n_jobs.py

--- helpers/n_jobs.py
+++ helpers/n_jobs.py
@@ -1,6 +1,12 @@
 # set n_jobs = cpu count 
 # to take advantage of parallel computing
 
+# n_jobs: Number of cores used for the training process
+# If set to -1, all cores will be used
+n_jobs = -1
+
+# alternately,
+
 import multiprocessing
 
 n_jobs = multiprocessing.cpu_count()
