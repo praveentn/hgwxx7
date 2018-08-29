ab7614816c27c24965e2379f1234e5ebe015bd50
Praveen Narayan <tnpraveen87@gmail.com>
2018-08-29 13:22:12 +0530

    Create plot_17.py

---
+++ helpers/plot_17.py
@@ -0,0 +1,61 @@
+# sample plottings
+# training dataframe; costarica dataset
+
+# getting object dtypes
+train.select_dtypes('object')
+
+# sample 1
+train = pd.DataFrame('train.csv')
+
+# plot the count of Unique Values in integer Columns
+train.select_dtypes(np.int64).nunique().value_counts().sort_index()
+            .plot.bar(color = 'red', figsize = (9, 6), edgecolor = 'k', linewidth = 2);
+plt.xlabel('Number of Unique Values'); plt.ylabel('Count');
+plt.title('Count of Unique Values in Integer Columns');
+
+
+# sample 2
+# plotting different categories
+from collections import OrderedDict
+
+plt.figure(figsize = (20, 16))
+plt.style.use('fivethirtyeight')
+
+# Color mapping
+colors = OrderedDict({1: 'red', 2: 'orange', 3: 'blue', 4: 'green'})
+poverty_mapping = OrderedDict({1: 'extreme', 2: 'moderate', 3: 'vulnerable', 4: 'non vulnerable'})
+
+# Iterate through the float columns
+for i, col in enumerate(train.select_dtypes('float')):
+    ax = plt.subplot(4, 2, i + 1)
+    # Iterate through the poverty levels
+    for poverty_level, color in colors.items():
+        # Plot each poverty level as a separate line
+        sns.kdeplot(train.loc[train['Target'] == poverty_level, col].dropna(), 
+                    ax = ax, color = color, label = poverty_mapping[poverty_level])
+        
+    plt.title(f'{col.capitalize()} Distribution'); plt.xlabel(f'{col}'); plt.ylabel('Density')
+
+plt.subplots_adjust(top = 2)
+
+
+# sample 3
+# distribution by category
+
+plt.figure(figsize = (16, 12))
+
+# Iterate through the float columns
+for i, col in enumerate(['dependency', 'edjefa', 'edjefe']):
+    ax = plt.subplot(3, 1, i + 1)
+    # Iterate through the poverty levels
+    for poverty_level, color in colors.items():
+        # Plot each poverty level as a separate line
+        sns.kdeplot(train.loc[train['Target'] == poverty_level, col].dropna(), 
+                    ax = ax, color = color, label = poverty_mapping[poverty_level])
+        
+    plt.title(f'{col.capitalize()} Distribution'); plt.xlabel(f'{col}'); plt.ylabel('Density')
+
+plt.subplots_adjust(top = 2)
+
+
+
