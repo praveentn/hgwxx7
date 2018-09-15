7fb5af25e824ed2203ab3d68af9117eb4f0d7399
praveentn <tnpraveen87@gmail.com>
2018-09-15 12:10:32 +0530

    keyword

---
+++ python/language/keyword/keyword_kwlist.py
@@ -0,0 +1,20 @@
+'''
+E:\>Python
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 18:11:49) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license" for more information.
+'''
+
+# load library
+import keyword
+
+# keyword
+keyword
+<module 'keyword' from 'E:\\Python\\lib\\keyword.py'>
+
+# list of keywords!
+keyword.kwlist
+['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+# length of keyword list
+len(keyword.kwlist)
+# 33
